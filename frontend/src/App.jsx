517c0421cf6492d6332291fc64d75edfb3a3591a
BhojGC <ghartich@sheridancollege.ca>
2025-06-25 23:29:39 -0400

    frontend initializaion complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import HomePage from "./pages/HomePage";
+import MainLayout from "./Layouts/MainLayout";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+     
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
